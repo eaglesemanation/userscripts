06df7df580da4f595e9bdc69842e82e3cc7c93b4
Vladimir Romashchenko <eaglesemanation@gmail.com>
2024-06-14 22:07:13 -0400

    Add licence tag

--- neofinancial-csv.user.js
+++ neofinancial-csv.user.js
@@ -4,6 +4,7 @@
 // @match       https://member.neofinancial.com/*
 // @grant       GM.xmlHttpRequest
 // @version     1.0
+// @license     MIT
 // @author      eaglesemanation
 // @description Adds a button to transactions page that exports all transactions into a CSV file. Developed for use with "Actual" budgeting tool, will probably work fine with any other importer.
 // ==/UserScript==
